e6a0d034b9141012227c95fd14b2baaf8ccfcce5
tang chin xing <tcx200306@gmail.com>
2025-09-14 22:10:48 +0800

    Adding Sample Data: categories,items,quantity,price of product

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Mini Online Shop")
+categories = ["Sports","Electronics","Furniture"]
+items = [["Yonex Badminton Racquet","Felet Badminton Racquet","Nike Runner Shoes Mark II","Puma Jogging Socks"],["iPhone 17 Pro Max 256GB","iPhone Air","Airpods Pro 3","Samsung Z Fold 7 1TB"],["Ikea Essential Chair","3A Plastic Chair","Foldable Table"]]
+quantity = [[59,65,90,30],[200,582,569,223],[477,896,452]]
+price = [[200,250,500,90],[5999,4999,999,9999],[59,120,299]]
+
+while True:
+    print("=== Mini Online Shop ===")
+    
+    
